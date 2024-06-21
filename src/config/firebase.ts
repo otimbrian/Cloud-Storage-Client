0f39df4c3500ddffa17b3523ba8652049d5d8519
otimbrian <otims.brian@gmail.com>
2024-06-21 18:38:57 +0300

    security

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -3,15 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getStorage } from "firebase/storage"
 import {getFirestore} from "firebase/firestore"
 
-const firebaseConfig = {
-  apiKey: "AIzaSyD6ImXcfgCYxuMJa2SsO3lNzVVfR_H3pHs",
-  authDomain: "new-drop-box.firebaseapp.com",
-  projectId: "new-drop-box",
-  storageBucket: "new-drop-box.appspot.com",
-  messagingSenderId: "260802662079",
-  appId: "1:260802662079:web:7cd146a5fb848c1d7fc00a",
-  measurementId: "G-NLPFN0P1EX"
-};
+
 
 
 const app = initializeApp(firebaseConfig);
